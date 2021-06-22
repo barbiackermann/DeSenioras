783da4a4c245620ceacedf3c4c0ebb943d8fa6aa
BarbiAckermann <barbiackermann@gmail.com>
2021-06-22 20:06:14 -0300

    add banners

--- src/Components/Ofrecemos/Ofrecemos.js
+++ src/Components/Ofrecemos/Ofrecemos.js
@@ -1,10 +1,9 @@
 import "./Ofrecemos.css";
 import { Container, Row, Col } from "react-bootstrap";
-import Sofrecemos1 from "./../IMG/Sofrecemos1.png";
-import Sofrecemos2 from "./../IMG/Sofrecemos2.png";
-import Sofrecemos3 from "./../IMG/Sofrecemos3.png";
-import Sofrecemos4 from "./../IMG/Sofrecemos4.png";
-import LineaDirecta from "./../LineaDirecta";
+import seniora1 from "./../IMG/seniora1.png";
+import seniora2 from "./../IMG/seniora2.png";
+import seniora3 from "./../IMG/seniora3.png";
+import seniora4 from "./../IMG/seniora4.png";
 
 const Ofrecemos = () => {
   return (
@@ -18,70 +17,49 @@ const Ofrecemos = () => {
         </Row>
         <Row className="offer-description">
           <Col md={3} className="offer-col">
-            <img
-              src={Sofrecemos1}
-              fluid
-              className="offer-icon"
-              alt="Pack 1"
-            ></img>
+            <img src={seniora1} fluid className="offer-icon" alt="Pack 1"></img>
             <h6>SEÑORA ESTELA</h6>
             <ul>
               <li>
-                Asesorías para armar la estrategia y planificación de redes.
+                ✔ Asesorías para armar la estrategia y planificación de redes.
               </li>
-              <li>Asesoramiento para el armado de tienda web.</li>
-              <li>Asesoramiento en el diseño de la identidad.</li>
+              <li>✔ Asesoramiento para el armado de tienda web.</li>
+              <li>✔ Asesoramiento en el diseño de la identidad.</li>
             </ul>
             <h3 className="loquieroEX">LO QUIERO</h3>
           </Col>
           <Col md={3} className="offer-col">
-            <img
-              src={Sofrecemos2}
-              fluid
-              className="offer-icon"
-              alt="PAck 2"
-            ></img>
+            <img src={seniora2} fluid className="offer-icon" alt="PAck 2"></img>
             <h6>SEÑORA MARTHA</h6>
             <ul>
-              <li>Tienda online en plataforma de ecommerce.</li>
-              <li>Diseño de Banners e imagenes para la tienda.</li>
-              <li>Diseño de isologo.</li>
-              <li>Estrategia y planificación en redes sociales.</li>
+              <li>✔ Tienda online en plataforma de ecommerce.</li>
+              <li>✔ Diseño de Banners e imagenes para la tienda.</li>
+              <li>✔ Diseño de isologo.</li>
+              <li>✔ Estrategia y planificación en redes sociales.</li>
             </ul>
             <h3 className="loquieroEX">LO QUIERO</h3>
           </Col>
           <Col md={3} className="offer-col">
-            <img
-              src={Sofrecemos3}
-              fluid
-              className="offer-icon"
-              alt="Pack 3"
-            ></img>
+            <img src={seniora3} fluid className="offer-icon" alt="Pack 3"></img>
             <h6>SEÑORA BEATRIZ</h6>
             <ul>
-              <li>Landing page a medida.</li>
-              <li>Diseño de isologotico con manual de marca.</li>
-              <li>Estrategia y planificacion de redes sociales.</li>
+              <li>✔ Landing page a medida.</li>
+              <li>✔ Diseño de isologotico con manual de marca.</li>
+              <li>✔ Estrategia y planificacion de redes sociales.</li>
             </ul>
             <h3 className="loquieroEX">LO QUIERO</h3>
           </Col>
           <Col md={3} className="offer-col">
-            <img
-              src={Sofrecemos4}
-              fluid
-              className="offer-icon"
-              alt="Pack 4"
-            ></img>
+            <img src={seniora4} fluid className="offer-icon" alt="Pack 4"></img>
             <h6>SEÑORA GLORIA</h6>
             <ul>
-              <li>Sitio Full o Standard diseñado a medida.</li>
-              <li>Diseño de isologo y manual de marca.</li>
-              <li>Estrategia y planificacion en redes sociales.</li>
+              <li>✔ Sitio Full o Standard diseñado a medida.</li>
+              <li>✔ Diseño de isologo y manual de marca.</li>
+              <li>✔ Estrategia y planificacion en redes sociales.</li>
             </ul>
             <h3 className="loquieroEX">LO QUIERO</h3>
           </Col>
         </Row>
-        <LineaDirecta></LineaDirecta>
       </Container>
     </>
   );
